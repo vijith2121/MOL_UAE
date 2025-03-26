9085c3e0bd9ded500fd8ed3e7ac62b1ceec7e85b
vijith2121 <vijisaj2121@gmail.com>
2025-03-26 09:21:31 +0530

    update input

--- MOL_UAE/spiders/main.py
+++ MOL_UAE/spiders/main.py
@@ -40,7 +40,7 @@ class Mol_uaeSpider(scrapy.Spider):
             'passportNo': 'F5110797',
         }
         script_directory = os.path.dirname(os.path.abspath(__file__))
-        file_path = os.path.join(script_directory, "split_part_3.csv")
+        file_path = os.path.join(script_directory, "split_part_4.csv")
         df = pd.read_csv(file_path)
         for item in df.to_dict('records'):
             passport_no = item.get('Passport No.').replace(' ', '').strip()
